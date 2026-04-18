d444cd5719d1d01d60ea123aa0e7022e6dbca73d
Miroslaw <witcherofthorns@gmail.com>
2026-04-18 23:22:04 +0300

    yookassa create receipt for payment object, replace payment method 'bank_card' on 'sbp'

--- src/services/yookassa.js
+++ src/services/yookassa.js
@@ -15,6 +15,56 @@ export const createPayment = async ({ amount, userId }) => {
             currency: 'RUB'
         },
         payment_method_data: {
+            type: 'sbp'
+        },
+        confirmation: {
+            type: 'redirect',
+            return_url: process.env.SCANHAND_YOO_REDIRECT
+        },
+        metadata: {
+            userId
+        },
+        receipt: {
+            customer: {
+                email: "gv.yatcunov@gmail.com"
+            },
+            items: [
+                {
+                    description: "Пополнение кредитов",
+                    quantity: 1.000,
+                    amount: {
+                        value: `${amount}.00`,
+                        currency: "RUB"
+                    },
+                    vat_code: 1,
+                    payment_mode: "full_prepayment",
+                    payment_subject: "commodity"
+                }
+            ],
+            internet: true,
+            timezone: 2
+        }
+    };
+
+    try {
+        const payment = await checkout.createPayment(createPayload, idempotenceKey);
+        return payment;
+    }
+    catch (error) {
+        console.error(error);
+        return null
+    }
+};
+
+export const createPaymentTest = async ({ amount, userId }) => {
+    const idempotenceKey = uuidv4();
+    const createPayload = {
+        capture: true,
+        amount: {
+            value: `${amount}.00`,
+            currency: 'RUB'
+        },
+        payment_method_data: {
             type: 'bank_card'
         },
         confirmation: {
@@ -50,7 +100,6 @@ export const getPaymentStatus = async (paymentId) => {
         const payment = await checkout.getPayment(paymentId);
         return payment;
     } catch (error) {
-        console.error(error);
         return null;
     }
 };
